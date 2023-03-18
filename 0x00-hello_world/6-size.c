c01d63bb989b93a05d6c401382ed07a941291149
ISRAEL254 <israelmugisha298@gmail.com>
2023-03-17 17:35:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
- * main - program that prints the size of various types on the computer 
+ * main - program that prints the size of various types on the comp
  * Return: 0 (success)
  */
-
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
