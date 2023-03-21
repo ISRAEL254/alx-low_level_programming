ad01c785b4a9e47b5e8846c5f511da8ce4a0cdec
ISRAEL254 <israelmugisha298@gmail.com>
2023-03-20 18:13:14 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a, d;
+
+	for (a = 0; a <= 8; a++) {
+		for (d = a + 1; d <= 9; d++) {
+			putchar(a + '0');
+			putchar(d + '0');
+			if (a < 8) {
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
