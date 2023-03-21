0bf0b50b8dc3b65caef94fdbafd61234094543e3
ISRAEL254 <israelmugisha298@gmail.com>
2023-03-20 18:24:44 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,19 @@ int main(void)
 {
 	int a, d;
 
-	for (a = 0; a <= 8; a++) {
-		for (d = a + 1; d <= 9; d++) {
+	for (a = 0; a <= 8; a++)
+	{
+		for (d = a + 1; d <= 9; d++)
+		{
 			putchar(a + '0');
 			putchar(d + '0');
-			if (a < 8) {
+			if (a < 8)
+			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n')
 	return (0);
 }
