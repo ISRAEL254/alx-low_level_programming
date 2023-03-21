9012ce6f5eb3a1ea4fe269e61e0c2f69ed237a7f
ISRAEL254 <israelmugisha298@gmail.com>
2023-03-21 15:03:09 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
