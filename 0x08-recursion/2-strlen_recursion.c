9bd19e37a1024fb8fafacb78057dc1bca264d551
ISRAEL254 <israelmugisha298@gmail.com>
2023-04-06 17:17:41 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,17 @@
 
 /**
  * _strlen_recursion -  a function that returns the length of a string
- * @s: string
- * Return: int
+ * @s:string
+ * Return:int
  */
 int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	while (*s)
+	if (*s)
 	{
 		i++;
-		s++;
+		i += _strlen_recursion(s + 1);
 	}
 
 	return (i);
