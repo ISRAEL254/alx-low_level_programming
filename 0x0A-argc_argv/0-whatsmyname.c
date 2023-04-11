cc9e48c6a1a255878fc30d04eb088880ee25e2f2
ISRAEL254 <israelmugisha298@gmail.com>
2023-04-10 18:36:21 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
-#include <studio.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - a program tthat prints its name, followed by new line.
+ * main - a program that prints its name, followed by new line.
  * @argc: argument count
  * @argv: arguments
  *
@@ -12,7 +13,7 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 
-	printf("%s\n", argv[0});
-	retunr (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
 
