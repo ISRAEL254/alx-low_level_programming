1fc3b8170fe37cbe5d43b14689efc13ae147a3c8
ISRAEL254 <israelmugisha298@gmail.com>
2023-04-10 18:16:49 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+/**
+ * main - a program tthat prints its name, followed by new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0});
+	retunr (0);
+}
+
