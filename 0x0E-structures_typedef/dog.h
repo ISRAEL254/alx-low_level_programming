9a17874131e4cf2b46d5ae8f9432f2dcce5ce7b8
ISRAEL254 <israelmugisha298@gmail.com>
2023-04-18 14:39:26 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,11 +4,10 @@
 
 /**
  * struct dog - a dog's basic info
- * @name:name of the dog
+ * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Description: Longer description
  */
 
 struct dog
@@ -18,9 +17,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
